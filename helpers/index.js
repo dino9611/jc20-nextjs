021f3745d8b5f7af1cec5af6c2b5a7b3cab3dc9f
dino9611 <aldinorahman36@gmail.com>
2022-03-18 11:30:56 +0700

    day3be

--- helpers/index.js
+++ helpers/index.js
@@ -1,1 +1,6 @@
-export const API_URL = `http://localhost:5000`;
+import axios from "axios";
+const API_URL = `http://localhost:5000`;
+
+export const AxiosInstance = axios.create({
+  baseURL: API_URL,
+});
