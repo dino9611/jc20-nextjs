6353ef93fb02f7884fab367bc8f39aa405bcec00
dino9611 <aldinorahman36@gmail.com>
2022-03-18 17:38:29 +0700

    sesi2 day3be

--- pages/index.js
+++ pages/index.js
@@ -14,19 +14,31 @@ import {
   DialogTitle,
   DialogContent,
 } from "@mui/material";
+import Swal from "sweetalert2";
 // export const getStaticProps = async () => {
 
 // };
+// [
+//   {postingan_id:12,postingan:'',userposting:'',userpostingphoto:'',imageposting:'',likes:200,comment_total:100,likebyuser:true}
+// ]
 
 export default function Home(props) {
   const [data, setdata] = useState([]);
   const [isOpen, setisOpen] = useState(false);
+  const [isOpenEdit, setisOpenEdit] = useState(false);
+  const [indexEdit, setindexEdit] = useState(-1);
   const [input, setinput] = useState({
+    name: "",
+    price: "",
+  });
+  const [inputEdit, setinputEdit] = useState({
     name: "",
     price: "",
   });
 
   const nameref = useRef(null);
+  const priceref = useRef(null);
+  const priceEditref = useRef(null);
 
   const fetchData = async () => {
     try {
@@ -37,6 +49,9 @@ export default function Home(props) {
       console.log(error);
     }
   };
+  useEffect(() => {
+    console.log(nameref.current);
+  }, [nameref]);
 
   useEffect(() => {
     fetchData();
@@ -45,9 +60,79 @@ export default function Home(props) {
   const handleInput = (e) => {
     setinput({ ...input, [e.target.name]: e.target.value });
   };
+  const handleInputEdit = (e) => {
+    setinputEdit({ ...inputEdit, [e.target.name]: e.target.value });
+  };
+
+  const addDataHandler = async () => {
+    try {
+      let res = await AxiosInstance.post("/product", input);
+      setdata(res.data);
+      setinput({
+        name: "",
+        price: "",
+      });
+      setisOpen(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleKeyUp = (e, tesref) => {
+    console.log(e.keyCode);
+    if (e.keyCode === 13 && e.target.name === "price") {
+      console.log("masuk sini");
+      addDataHandler();
+    } else if (e.keyCode === 13) {
+      tesref.current.focus();
+    }
+  };
+
+  const handleKeyUpEdit = (e, tesref) => {
+    console.log(e.keyCode);
+    if (e.keyCode === 13 && e.target.name === "price") {
+      console.log("masuk sini");
+      onSaveEditCLick();
+      // function edit yang ada axios
+    } else if (e.keyCode === 13) {
+      tesref.current.focus();
+    }
+  };
+
+  const handleDelete = async (id, index) => {
+    try {
+      const result = await Swal.fire({
+        title: `Are you sure want to delete ${data[index].name} ?`,
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      });
+      if (result.isConfirmed) {
+        let res = await AxiosInstance.delete("/product/" + id);
+        setdata(res.data);
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const onEditCLick = (index) => {
+    setindexEdit(index);
+    setisOpenEdit(true);
+    setinputEdit(data[index]);
+  };
+
+  const onSaveEditCLick = () => {
+    // disini axios
+    console.log(inputEdit);
+  };
 
   const renderData = () => {
-    return data.map((val) => {
+    return data.map((val, index) => {
       return (
         <div
           className="bg-white text-matoa-text rounded-md my-4 p-3 shadow-md w-1/2"
@@ -60,10 +145,16 @@ export default function Home(props) {
           <div className="mt-2 ">
             <div className="text-md text-lg">{val.price}</div>
             <div className="float-right">
-              <button className="bg-matoa-text-primary mr-2 w-[70px] p-2 rounded-md text-white">
+              <button
+                onClick={() => handleDelete(val.id, index)}
+                className="bg-matoa-text-primary mr-2 w-[70px] p-2 rounded-md text-white"
+              >
                 Delete
               </button>
-              <button className="bg-matoa-text-primary w-[70px] p-2 rounded-md text-white">
+              <button
+                onClick={() => onEditCLick(index)}
+                className="bg-matoa-text-primary w-[70px] p-2 rounded-md text-white"
+              >
                 Edit
               </button>
             </div>
@@ -73,32 +164,77 @@ export default function Home(props) {
     });
   };
 
+  const renderForm = (inputpar, handleInputpar, handleKeyUppar, refpar) => {
+    return (
+      <>
+        <input
+          onChange={handleInputpar}
+          // ref={nameref}
+          onKeyUp={(e) => handleKeyUppar(e, refpar)}
+          // onFocus={() => console.log("focus")}
+          // onBlur={() => console.log("blur")}
+          autoFocus
+          value={inputpar.name}
+          type="text"
+          className="my-1 w-full focus:ring-matoa-text-primary focus:border-matoa-text-primary"
+          name="name"
+          placeholder="name"
+        />
+        <input
+          onChange={handleInputpar}
+          onKeyUp={(e) => handleKeyUppar(e, null)}
+          ref={refpar}
+          value={inputpar.price}
+          type="number"
+          className="my-1 w-full focus:ring-matoa-text-primary focus:border-matoa-text-primary"
+          name="price"
+          placeholder="price"
+        />
+      </>
+    );
+  };
+
   return (
     <Container>
       <Dialog open={isOpen} onClose={() => setisOpen(false)}>
         <DialogTitle>ADD data Product</DialogTitle>
         <DialogContent>
-          <input
-            onChange={handleInput}
-            ref={nameref}
-            value={input.name}
-            type="text"
-            className="my-1 w-full focus:ring-matoa-text-primary focus:border-matoa-text-primary"
-            name="name"
-            placeholder="name"
-          />
-          <input
-            onChange={handleInput}
-            value={input.price}
-            type="number"
-            className="my-1 w-full focus:ring-matoa-text-primary focus:border-matoa-text-primary"
-            name="price"
-            placeholder="price"
-          />
+          {renderForm(input, handleInput, handleKeyUp, priceref)}
         </DialogContent>
         <DialogActions>
           <button
+            onClick={addDataHandler}
+            className="bg-matoa-text-primary min-w-[80px] p-2 rounded-md text-white"
+          >
+            Save
+          </button>
+          <button
             onClick={() => setisOpen(false)}
+            className="bg-matoa-text-primary min-w-[80px] p-2 rounded-md text-white"
+          >
+            Cancel
+          </button>
+        </DialogActions>
+      </Dialog>
+      <Dialog open={isOpenEdit} onClose={() => setisOpenEdit(false)}>
+        <DialogTitle>edit data {data[indexEdit]?.name}</DialogTitle>
+        <DialogContent>
+          {renderForm(
+            inputEdit,
+            handleInputEdit,
+            handleKeyUpEdit,
+            priceEditref
+          )}
+        </DialogContent>
+        <DialogActions>
+          <button
+            onClick={onSaveEditCLick}
+            className="bg-matoa-text-primary min-w-[80px] p-2 rounded-md text-white"
+          >
+            Save
+          </button>
+          <button
+            onClick={() => setisOpenEdit(false)}
             className="bg-matoa-text-primary min-w-[80px] p-2 rounded-md text-white"
           >
             Cancel
@@ -108,7 +244,7 @@ export default function Home(props) {
       <div>
         <button
           onClick={() => {
-            // console.log(nameref.current);
+            // nameref.current.focus();
             setisOpen(true);
             // nameref.current.focus();
           }}
